7a0db06f90c49b73e099239cbe34689f41296bfb
anthony mcmullen <amcmullen9291@gmail.com>
2021-02-08 05:25:57 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,5 @@ class Song < ApplicationRecord
   validates_presence_of :title, null: false
   validates_presence_of :released 
   validates_presence_of :release_year, null: false if -> {:released == "true"}
-  validates :release_year, numericality: {less_than_or_equal_to: -> { Date.current.year } }
+  validates :release_year, on_or_before: lambda { Date.current }
 end
